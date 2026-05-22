b945826d7027e8002c73f8528fa5a899c34cb819
Daniel Q <daniel04@gmail.com>
2026-05-22 18:07:46 -0400

    Fix. Configs

--- tiendaweb-frontend/src/app/app.config.ts
+++ tiendaweb-frontend/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -6,6 +10,7 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection(),
     provideRouter(routes),
     provideHttpClient()
   ]
